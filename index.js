283b0e8d7c0431706cceae450964fae8367a5a21
varun sachdeva <sachdevavarun7@gmai.com>
2020-11-05 02:59:09 +0530

    add get routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+// requiring and using NPM packages
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
@@ -14,6 +16,7 @@ app.use(express.static("public"));
 
 app.use(logger('dev'));
 
+// routes 
 app.get('/', (req, res) => {
     try{
         res.render('index');
@@ -28,13 +31,56 @@ app.get('/aboutus', (req,res) => {
     res.render('aboutus');
 })
 
-app.get('/users', (req,res) => {
-    res.render('users');
+app.get('/register', (req,res) => {
+    res.render('registers');
 })
+
+app.get('/login', (req,res) => {
+
+    res.render('login');
+})
+
+app.get('/organization', (req,res) => {
+
+    res.render('organiztion');
+})
+
+app.get('/organization/manageCurrent', (req,res) => {
+
+    res.render('manageCurrentEvents');
+})
+
+app.get('/organization/manageCurrent/:eventId', (req,res) => {
+
+    res.render('events');
+})
+
+app.get('/organization/pastEvents', (req,res) => {
+
+    res.render('pastEvents');
+})
+
+app.get('/organization/manageProfile', (req,res) => {
+
+    res.render('manageProfile');
+})
+
+app.get('/organization/create', (req,res) => {
+
+    res.render('reateEvent');
+})
+
 app.post('/contact', (req,res)=> {
     console.log(req.body);
     res.redirect('back'); 
 })
+
+app.get('/aboutus', (req,res) => {
+
+    res.render('aboutus');
+})
+
+// listen on local host or service host
 
 app.listen(port, () => {
     console.log('Connected to the port: '+ port);
