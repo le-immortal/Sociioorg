736d9832d5beccca075232d58e4d305866d4de35
varun sachdeva <sachdevavarun7@gmai.com>
2020-11-05 03:03:55 +0530

    get routes

--- index.js
+++ index.js
@@ -29,59 +29,64 @@ app.get('/', (req, res) => {
 app.get('/aboutus', (req,res) => {
 
     res.render('aboutus');
-})
+});
+
+app.get('/users', (req,res) => {
+
+    res.render('users');
+});
 
 app.get('/register', (req,res) => {
     res.render('registers');
-})
+});
 
 app.get('/login', (req,res) => {
 
     res.render('login');
-})
+});
 
 app.get('/organization', (req,res) => {
 
     res.render('organiztion');
-})
+});
 
 app.get('/organization/manageCurrent', (req,res) => {
 
     res.render('manageCurrentEvents');
-})
+});
 
 app.get('/organization/manageCurrent/:eventId', (req,res) => {
 
     res.render('events');
-})
+});
 
 app.get('/organization/pastEvents', (req,res) => {
 
     res.render('pastEvents');
-})
+});
 
 app.get('/organization/manageProfile', (req,res) => {
 
     res.render('manageProfile');
-})
+});
 
 app.get('/organization/create', (req,res) => {
 
     res.render('reateEvent');
-})
+});
 
 app.post('/contact', (req,res)=> {
     console.log(req.body);
     res.redirect('back'); 
-})
+});
 
 app.get('/aboutus', (req,res) => {
 
     res.render('aboutus');
-})
+});
 
 // listen on local host or service host
 
 app.listen(port, () => {
     console.log('Connected to the port: '+ port);
-})
+});
